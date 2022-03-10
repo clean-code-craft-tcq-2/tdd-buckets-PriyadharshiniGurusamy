db86bffa181f9685f69ccbd0d6d9857bb31dfa34
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-03-10 23:15:00 +0530

    Update TestChargeRange.cpp

--- TestChargeRange.cpp
+++ TestChargeRange.cpp
@@ -7,7 +7,10 @@ TEST_CASE("Check readings for 3 current samples")
 {
   ChargeRangeCheck obj;
   obj.getRange({1,1,2});
-  REQUIRE(obj.v_rangeCount.second == 3);
+  for (RangeCount::iterator itr = obj.v_rangeCount.begin(); itr != obj.v_rangeCount.end(); itr++)
+  {
+     REQUIRE((*itr).second == 3);
+  }
 }
 
 //TestgetRange(std::vector input, ChargeRangeCheck& obj)
