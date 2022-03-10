1bb720a87c76321b0770bedeebee085b207c7c38
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-03-10 22:19:50 +0530

    Create ChareRangeCheckDefines.h

---
+++ ChareRangeCheckDefines.h
@@ -0,0 +1,10 @@
+#ifndef CHARGECHECKDEFINES_H
+#define CHARGECHECKDEFINES_H
+
+#include <vector>
+
+typedef std::pair<int, int> Range;
+typedef std::pair<Range, int> RangeCountPair;
+typedef std::vector<RangeCountPair> RangeCount;
+
+#endif
