8203bebdc5f309211edfc145e2e761f3f4560db3
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-03-11 14:18:15 +0530

    Update TestChargeRange.cpp

--- TestChargeRange.cpp
+++ TestChargeRange.cpp
@@ -3,17 +3,18 @@
 #include "test/catch.hpp"
 #include "ChargeRangeCheck.h"
 
+
+void AssetRangeandReadings(RangeCountPair pair, int minVal, int maxVal, int readings)
+{
+  REQUIRE(pair.second == readings);
+  REQUIRE(pair.first.first == minVal);
+  REQUIRE(pair.first.second == maxVal);  
+}
+
 TEST_CASE("Check readings for 3 current samples") 
 {
   ChargeRangeCheck obj;
   obj.getRangeandReadings({1,1,2});
   
   AssetRangeandReadings (obj.v_rangeCount[0], 1,2,3);
-}
-
-AssetRangeandReadings(RangeCountPair pair, int minVal, int maxVal, int readings)
-{
-  REQUIRE(pair.second == readings);
-  REQUIRE(pair.first.first == minVal);
-  REQUIRE(pair.first.second == maxVal);  
 }
