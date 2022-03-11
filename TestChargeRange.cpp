77aea6b19242b19b570060063fac621526851274
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-03-11 14:22:49 +0530

    Update TestChargeRange.cpp

--- TestChargeRange.cpp
+++ TestChargeRange.cpp
@@ -11,10 +11,31 @@ void AssetRangeandReadings(RangeCountPair pair, int minVal, int maxVal, int read
   REQUIRE(pair.first.second == maxVal);  
 }
 
-TEST_CASE("Check readings for 3 current samples") 
+TEST_CASE("Check readings for 3 consecutive current samples") 
 {
   ChargeRangeCheck obj;
   obj.getRangeandReadings({1,1,2});
   
   AssetRangeandReadings (obj.v_rangeCount[0], 1,2,3);
+}
+
+TEST_CASE("Check readings for 5 inconsecutive current samples") 
+{
+  ChargeRangeCheck obj;
+  obj.getRangeandReadings({1,1,2,4,5});
+  
+  AssetRangeandReadings (obj.v_rangeCount[0], 1,2,3);
+  AssetRangeandReadings (obj.v_rangeCount[1], 4,5,2);
+}
+
+
+TEST_CASE("Check readings for 7 inconsecutive current samples") 
+{
+  ChargeRangeCheck obj;
+  obj.getRangeandReadings({2,4,5,4,9,7,5});
+  
+  AssetRangeandReadings (obj.v_rangeCount[0], 2,2,1);
+  AssetRangeandReadings (obj.v_rangeCount[1], 4,5,4);
+  AssetRangeandReadings (obj.v_rangeCount[2], 7,7,1);
+  AssetRangeandReadings (obj.v_rangeCount[3], 9,9,1);
 }
